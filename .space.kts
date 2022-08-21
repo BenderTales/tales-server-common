a8019394f9cfa89010f2199f3b934773fe4d3564
Noko O <deboisjerome@gmail.com>
2022-08-21 18:16:14 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,16 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Publish package on new version tag") {
+	startOn {
+        gitPush {
+            branchFilter {
+                +"refs/heads/release-*"
+            }
+            
+        }
+    }
+}
